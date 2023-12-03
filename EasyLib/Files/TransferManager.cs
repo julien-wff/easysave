89230be6c54450de1141469bd498c119587eb495
Pow3y <lukas.houille@viacesi.fr>
2023-12-03 21:21:08 +0100

    feat: TransferManager SourceScan

---
+++ EasyLib/Files/TransferManager.cs
@@ -0,0 +1,45 @@
+﻿
+namespace EasyLib.Files;
+
+/// <summary>
+/// this class is used to handle the transfer of files from a source to a destination
+/// </summary>
+public class TransferManager
+{
+    public Job.Job Job;
+    private BackupFolder _sourceFolder;
+    private BackupFolder _destinationFolder;
+    
+    public TransferManager(Job.Job job)
+    {
+        Job = job;
+        _sourceFolder = new BackupFolder(Job.SourceFolder);
+        _destinationFolder = new BackupFolder(Job.DestinationFolder);
+    }
+    /// <summary>
+    /// Update the instance source folder and the files count and size from the job instance
+    /// </summary>
+    public void ScanSource()
+    {
+        _sourceFolder.Walk(Job.SourceFolder);
+        Job.FilesCount = 0;
+        Job.FilesSizeBytes = 0;
+        _getFileInfo(_sourceFolder);
+    }
+    /// <summary>
+    /// Used in ScanSource to go trough the file tree and get the size and number of files
+    /// </summary>
+    /// <param name="folder"></param>
+    private void _getFileInfo(BackupFolder folder)
+    {
+        foreach (var file in folder.Files)
+        {
+            Job.FilesSizeBytes += file.Size;
+            Job.FilesCount++;
+        }
+        foreach (var subFolder in folder.SubFolders)
+        {
+            _getFileInfo(subFolder);
+        }
+    }
+}
---
+++ EasySave.Tests/EasyLib/Files/TransferManagerTests.cs
@@ -0,0 +1,38 @@
+﻿using EasyLib.Enums;
+using EasyLib.Files;
+using EasyLib.Job;
+
+namespace EasySave.Tests.EasyLib.Files;
+
+public class TransferManagerTests
+{
+    [Fact]
+    public void TestTransferManagerSourceSan()
+    {
+        // Arrange
+        string tempDirPath = Path.GetTempPath();
+        Directory.CreateDirectory(tempDirPath + @"TransferManagerTests\");
+        string testPath = @"TransferManagerTests\";
+        Directory.CreateDirectory(tempDirPath + testPath + @"SourcePath\");
+        Directory.CreateDirectory(tempDirPath + testPath + @"SourcePath\dir1\");
+        Directory.CreateDirectory(tempDirPath + testPath + @"SourcePath\dir2\");
+        Directory.CreateDirectory(tempDirPath + testPath + @"SourcePath\dir1\dir3\");
+        File.WriteAllText(tempDirPath + testPath + @"SourcePath\file0.txt", "fil0");
+        File.WriteAllText(tempDirPath + testPath + @"SourcePath\dir1\file1.txt", "file1");
+        File.WriteAllText(tempDirPath + testPath + @"SourcePath\dir2\fil2.txt", "file2");
+        File.WriteAllText(tempDirPath + testPath + @"SourcePath\dir1\dir3\file3.txt", "file3");
+        
+        const string jobName = "job1";
+        string sourcePath = tempDirPath + testPath + @"SourcePath\";
+        string destinationPath = tempDirPath + testPath + @"DestinationPath\";
+        const JobType jobType = JobType.Full;
+
+        // Act
+        var job = new Job(jobName, sourcePath, destinationPath, jobType);
+        var transferManager = new TransferManager(job);
+        transferManager.ScanSource();
+        
+        // Assert
+        Assert.Equal((uint)4, job.FilesCount);
+    }
+}
