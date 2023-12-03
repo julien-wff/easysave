b22a5617500571cfe6541985cd20bdf9b9d0143f
Pow3y <lukas.houille@viacesi.fr>
2023-12-03 19:00:18 +0100

    feat: BackupFolders and tests

---
+++ EasyLib/Files/BackupFolders.cs
@@ -0,0 +1,44 @@
+﻿namespace EasyLib.Files;
+
+/// <summary>
+/// Recursively reproduce the file tree given by the file path
+/// </summary>
+public class BackupFolder
+{
+    public readonly string Name;
+    public List<BackupFolder> SubFolders;
+    public List<BackupFile> Files;
+    /// <summary>
+    /// Constructor of the BackupFolder class
+    /// </summary>
+    /// <param name="path"></param>
+    public BackupFolder(string path)
+    {
+        this.Name = Path.GetFileName(Path.GetDirectoryName(path));
+        this.SubFolders = new();
+        this.Files = new();
+    }
+    /// <summary>
+    /// This method recursively walks through the file tree
+    /// </summary>
+    /// <param name="path"></param>
+    public void Walk(string path)
+    {
+        var directoryInfo = new DirectoryInfo(path);
+        var subDirectories = directoryInfo.GetDirectories();
+        var files = directoryInfo.GetFiles();
+        
+        foreach (var subDirectory in subDirectories)
+        {
+            var backupFolder = new BackupFolder(subDirectory.FullName+@"\");
+            backupFolder.Walk(subDirectory.FullName);
+            SubFolders.Add(backupFolder);
+        }
+        
+        foreach (var file in files)
+        {
+            var backupFile = new BackupFile(file.FullName);
+            Files.Add(backupFile);
+        }
+    }
+}
---
+++ EasySave.Tests/EasyLib/Files/BackupFoldersTests.cs
@@ -0,0 +1,38 @@
+﻿using EasyLib.Files;
+
+namespace EasySave.Tests.EasyLib.Files;
+
+public class BackupFoldersTests
+{
+    [Fact]
+    public void TestBackupFolder()
+    {
+        // Arrange
+        string tempDirPath = Path.GetTempPath();
+        Directory.CreateDirectory(tempDirPath + @"BackupFolderTests\");
+        tempDirPath += @"BackupFolderTests\";
+        var backupFolder = new BackupFolder(tempDirPath);
+        string tempDirName = Path.GetFileName(Path.GetDirectoryName(tempDirPath));
+        Directory.CreateDirectory(tempDirPath + @"dir1\");
+        Directory.CreateDirectory(tempDirPath + @"dir2\");
+        Directory.CreateDirectory(tempDirPath + "dir1\\dir3\\");
+        File.WriteAllText(tempDirPath + "file0.txt", "test");
+        File.WriteAllText(tempDirPath + "dir1\\file1.txt", "file1");
+        File.WriteAllText(tempDirPath + "dir2\\fil2.txt", "file2");
+        File.WriteAllText(tempDirPath + "dir1\\dir3\\file3.txt", "file3");
+        
+        // Act
+        backupFolder.Walk(tempDirPath);
+
+        // Assert
+        Assert.Equal(tempDirName, backupFolder.Name);
+        Assert.Equal(2, backupFolder.SubFolders.Count);
+        Assert.Equal(1, backupFolder.SubFolders[0].SubFolders.Count);
+        Assert.Equal("dir1", backupFolder.SubFolders[0].Name);
+        Assert.Equal("dir3", backupFolder.SubFolders[0].SubFolders[0].Name);
+        Assert.Equal("file0.txt", backupFolder.Files[0].Name);
+
+        // Cleanup
+        Directory.Delete(tempDirPath, true);
+    }
+}
