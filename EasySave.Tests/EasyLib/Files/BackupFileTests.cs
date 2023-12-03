08654e2c75ad7acd596a976598cf88b3bd297846
Pow3y <lukas.houille@viacesi.fr>
2023-12-03 17:59:50 +0100

    feat: BackupFile and BackupFiletests

---
+++ EasySave.Tests/EasyLib/Files/BackupFileTests.cs
@@ -0,0 +1,29 @@
+﻿using EasyLib.Files;
+
+namespace EasySave.Tests.EasyLib.Files;
+
+public class BackupFileTests
+{
+    [Fact]
+    void TestBackupFile()
+    {
+        // Arrange
+        string tempFilePath = Path.GetTempFileName();
+        FileInfo file = new(tempFilePath);
+        string expectedName = Path.GetFileName(tempFilePath);
+        ulong expectedSize =(ulong) file.Length;
+
+        // Act
+        var backupFile = new BackupFile(tempFilePath);
+        var backupFile2 = new BackupFile(tempFilePath);
+
+        // Assert
+        Assert.Equal(expectedName, backupFile.Name);
+        Assert.Equal(expectedSize, backupFile.Size);
+        backupFile.Hash.Should().Be(backupFile2.Hash);
+
+        // Cleanup
+        File.Delete(tempFilePath);
+    }
+    
+}
