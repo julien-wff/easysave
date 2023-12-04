0b3fe1a5b204850d41a43e4caebfe189671beb85
Lukas H <lukas.houille@viacesi.fr>
2023-12-04 18:08:58 +0100

    feat: log manager

---
+++ EasyLib/Files/LogManager.cs
@@ -0,0 +1,35 @@
+﻿using EasyLib.Json;
+
+namespace EasyLib.Files;
+
+public class LogManager
+{
+    public readonly string LogFilePath;
+
+    private LogManager()
+    {
+        // AppData dir and append easysave/state.json
+        var appDataDir = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        var stateDirectory = Path.Combine(appDataDir, "easysave", "logs");
+        LogFilePath = Path.Combine(stateDirectory, DateTime.Now.ToString("yyyy-MM-dd") + ".json");
+
+        // Create directory if it doesn't exist
+        if (!Directory.Exists(stateDirectory))
+        {
+            Directory.CreateDirectory(stateDirectory);
+        }
+
+        // Create file and write [] if it doesn't exist
+        if (!File.Exists(LogFilePath))
+        {
+            File.WriteAllText(LogFilePath, "");
+        }
+    }
+
+    public static LogManager Instance { get; } = new LogManager();
+
+    public void AppendLog(JsonLogElement jsonLog)
+    {
+        JsonFileUtils.AppendJsonToList(LogFilePath, jsonLog);
+    }
+}
---
+++ EasyLib/Json/JsonLogElement.cs
@@ -0,0 +1,10 @@
+﻿namespace EasyLib.Json;
+
+public struct JsonLogElement
+{
+    public string JobName { get; set; }
+    public int TransferTime { get; set; }
+    public string SourcePath { get; set; }
+    public string DestinationPath { get; set; }
+    public ulong FileSize { get; set; }
+}
