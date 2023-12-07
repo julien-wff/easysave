ad914b8e31f8e066047551186d12d6445c10c76b
Julien W <50249422+julien-wff@users.noreply.github.com>
2023-12-07 09:16:57 +0100

    feat: gui app project

---
+++ EasyGUI/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿namespace EasyGUI;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App
+{
+}
---
+++ EasyGUI/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EasyGUI;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
