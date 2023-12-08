71377574b65e5ca4a45049649e8ba91efc00ee33
Lukas H <lukas.houille@viacesi.fr>
2023-12-08 10:31:35 +0100

    feat: easy crypto software

---
+++ EasyCrypto/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EasyCrypto;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var fileManager = new FileManager(args[0], "test");
+        fileManager.EncryptFile();
+    }
+}
