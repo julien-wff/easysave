7dacea0997476a3ca042141b954e6ee55e92452c
Julien W <50249422+julien-wff@users.noreply.github.com>
2023-12-08 10:47:00 +0100

    feat: tracing when the app crashes

--- EasyGUI/App.xaml.cs
+++ EasyGUI/App.xaml.cs
@@ -1,8 +1,48 @@
-﻿namespace EasyGUI;
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace EasyGUI;
 
 /// <summary>
 /// Interaction logic for App.xaml
 /// </summary>
 public partial class App
 {
+    public App()
+    {
+        Trace.Listeners.Add(new ConsoleTraceListener());
+        Trace.AutoFlush = true;
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        try
+        {
+            Trace.WriteLine("Application Starting.");
+            base.OnStartup(e);
+        }
+        catch (Exception ex)
+        {
+            Trace.WriteLine($"The application terminated unexpectedly during startup. Exception: {ex}");
+            throw;
+        }
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        try
+        {
+            Trace.WriteLine("Application Ended Successfully.");
+            base.OnExit(e);
+        }
+        catch (Exception ex)
+        {
+            Trace.WriteLine($"The application terminated unexpectedly during exit. Exception: {ex}");
+            throw;
+        }
+        finally
+        {
+            Trace.Close();
+        }
+    }
 }
