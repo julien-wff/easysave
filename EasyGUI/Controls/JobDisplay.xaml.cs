28b6a42044755ede8a37734bfe28eb45345c2cfa
Julien W <50249422+julien-wff@users.noreply.github.com>
2023-12-09 22:59:50 +0100

    feat: job breadcrumbs

--- EasyGUI/Controls/JobDisplay.xaml.cs
+++ EasyGUI/Controls/JobDisplay.xaml.cs
@@ -1,6 +1,8 @@
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows;
 using System.Windows.Data;
+using EasyLib.Enums;
 using EasyLib.Events;
 using EasyLib.Job;
 
@@ -18,28 +20,35 @@ public partial class JobDisplay : INotifyPropertyChanged, IJobStatusSubscriber
             bindingExpression?.UpdateTarget();
             OnPropertyChanged(nameof(Job));
             OnPropertyChanged(nameof(NameDisplay));
+            UpdateBreadcrumbs();
         };
     }
 
     public Job? Job => DataContext as Job;
 
-    public string NameDisplay
-    {
-        get
-        {
-            if (Job != null)
-            {
-                return $"#{Job.Id} - {Job.Name}";
-            }
-
-            return string.Empty;
-        }
-    }
+    public string NameDisplay => Job != null ? $"#{Job.Id} - {Job.Name}" : string.Empty;
 
     public event PropertyChangedEventHandler? PropertyChanged;
 
     protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
     {
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    private void UpdateBreadcrumbs()
+    {
+        SetBreadcrumbVisibility(FullBreadCrumb, Job?.Type == JobType.Full);
+        SetBreadcrumbVisibility(DifferentialBreadCrumb, Job?.Type == JobType.Differential);
+        SetBreadcrumbVisibility(IncrementalBreadCrumb, Job?.Type == JobType.Incremental);
+        SetBreadcrumbVisibility(EndBreadCrumb, Job?.State == JobState.End);
+        SetBreadcrumbVisibility(SourceBreadCrumb, Job?.State == JobState.SourceScan);
+        SetBreadcrumbVisibility(DiffCalcBreadCrumb, Job?.State == JobState.DifferenceCalculation);
+        SetBreadcrumbVisibility(StructureBreadCrumb, Job?.State == JobState.DestinationStructureCreation);
+        SetBreadcrumbVisibility(CopyBreadCrumb, Job?.State == JobState.Copy);
+    }
+
+    private static void SetBreadcrumbVisibility(UIElement breadCrumb, bool visible)
+    {
+        breadCrumb.Visibility = visible ? Visibility.Visible : Visibility.Collapsed;
     }
 }
