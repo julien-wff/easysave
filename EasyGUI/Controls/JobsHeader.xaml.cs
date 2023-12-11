4199a6370b77f798e5c03de7e70a185d8876c9f7
Julien W <50249422+julien-wff@users.noreply.github.com>
2023-12-11 13:49:23 +0100

    feat: header checkbox checks or unchecks all

--- EasyGUI/Controls/JobsHeader.xaml.cs
+++ EasyGUI/Controls/JobsHeader.xaml.cs
@@ -1,8 +1,12 @@
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows;
+using EasyLib.Job;
 
 namespace EasyGUI.Controls;
 
-public partial class JobsHeader
+public partial class JobsHeader : INotifyPropertyChanged
 {
     private static readonly RoutedEvent CreateButtonClickEvent = EventManager.RegisterRoutedEvent(
         nameof(CreateButtonClick),
@@ -11,10 +15,46 @@ public partial class JobsHeader
         typeof(JobsHeader)
     );
 
+    private static readonly DependencyProperty JobsProperty = DependencyProperty.Register(
+        nameof(Jobs),
+        typeof(ObservableCollection<Job>),
+        typeof(JobsHeader),
+        new PropertyMetadata(default(ObservableCollection<Job>))
+    );
+
+    private static readonly DependencyProperty SelectedJobsProperty = DependencyProperty.Register(
+        nameof(SelectedJobs),
+        typeof(ObservableCollection<Job>),
+        typeof(JobsHeader),
+        new PropertyMetadata(default(ObservableCollection<Job>))
+    );
+
     public JobsHeader()
     {
         InitializeComponent();
     }
+
+    public ObservableCollection<Job> Jobs
+    {
+        get => (ObservableCollection<Job>)GetValue(JobsProperty);
+        set
+        {
+            SetValue(JobsProperty, value);
+            OnPropertyChanged();
+        }
+    }
+
+    public ObservableCollection<Job> SelectedJobs
+    {
+        get => (ObservableCollection<Job>)GetValue(SelectedJobsProperty);
+        set
+        {
+            SetValue(SelectedJobsProperty, value);
+            OnPropertyChanged();
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
 
     public event RoutedEventHandler CreateButtonClick
     {
@@ -25,5 +65,29 @@ public partial class JobsHeader
     private void CreateButton_OnClick(object sender, RoutedEventArgs e)
     {
         RaiseEvent(new RoutedEventArgs(CreateButtonClickEvent));
+    }
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    private void ToggleButton_OnChecked(object sender, RoutedEventArgs e)
+    {
+        SelectedJobs.Clear();
+        foreach (var job in Jobs)
+        {
+            SelectedJobs.Add(job);
+        }
+    }
+
+    private void ToggleButton_OnUnchecked(object sender, RoutedEventArgs e)
+    {
+        SelectedJobs.Clear();
+    }
+
+    private void JobsHeader_OnLoaded(object sender, RoutedEventArgs e)
+    {
+        SelectedJobs.CollectionChanged += (_, _) => OnPropertyChanged(nameof(SelectedJobs));
     }
 }
