a572994678c5f0cb880e7ddf30ab64fd38d0abcb
Julien W <50249422+julien-wff@users.noreply.github.com>
2023-12-20 00:43:37 +0100

    feat: create a remote job from JsonJob

--- EasyLib/Job/RemoteJob.cs
+++ EasyLib/Job/RemoteJob.cs
@@ -1,10 +1,28 @@
 ﻿using EasyLib.Enums;
+using EasyLib.Json;
 
 namespace EasyLib.Job;
 
 public class RemoteJob(string name, string source, string destination, JobType type)
     : Job(name, source, destination, type)
 {
+    /// <summary>
+    /// Create a job instance from a JsonJob object
+    /// </summary>
+    /// <param name="job">JsonJob object</param>
+    public RemoteJob(JsonJob job) : this(job.name, job.source_folder, job.destination_folder, JobType.Full)
+    {
+        Id = job.id;
+        Type = EnumConverter<JobType>.ConvertToEnum(job.type);
+        State = EnumConverter<JobState>.ConvertToEnum(job.state);
+        FilesCount = job.active_job_info?.total_file_count ?? 0;
+        FilesSizeBytes = job.active_job_info?.total_file_size ?? 0;
+        FilesCopied = job.active_job_info?.files_copied ?? 0;
+        FilesBytesCopied = job.active_job_info?.bytes_copied ?? 0;
+        CurrentFileSource = job.active_job_info?.current_file_source ?? string.Empty;
+        CurrentFileDestination = job.active_job_info?.current_file_destination ?? string.Empty;
+    }
+
     public override bool Resume()
     {
         throw new NotImplementedException();
