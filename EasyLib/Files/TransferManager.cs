36e86734459f3f298b80887630ca4b95994ae9dc
Lukas H <lukas.houille@viacesi.fr>
2023-12-22 10:47:42 +0100

    fix: DifferentialBackupFolder

--- EasyLib/Files/TransferManager.cs
+++ EasyLib/Files/TransferManager.cs
@@ -81,12 +81,46 @@ public class TransferManager : IJobStatusPublisher
         InstructionsFolder = new BackupFolder(folders[1][0]);
         if (folders[2].Count != 0)
         {
-            InstructionsFolder.SubFolders.Add(new BackupFolder(folders[3][0]));
+            InstructionsFolder.SubFolders.Add(new BackupFolder(folders[1][0]));
             _compareBackupPath(InstructionsFolder.SubFolders[0], folders[0]);
+            folders[0].RemoveAt(folders[0].Count - 1);
         }
         else
         {
             _compareBackupPath(InstructionsFolder, folders[0]);
+        }
+    }
+
+    static void CopyDirectory(string sourceDir, string destinationDir, bool recursive)
+    {
+        // Get information about the source directory
+        var dir = new DirectoryInfo(sourceDir);
+
+        // Check if the source directory exists
+        if (!dir.Exists)
+            throw new DirectoryNotFoundException($"Source directory not found: {dir.FullName}");
+
+        // Cache directories before we start copying
+        DirectoryInfo[] dirs = dir.GetDirectories();
+
+        // Create the destination directory
+        Directory.CreateDirectory(destinationDir);
+
+        // Get the files in the source directory and copy to the destination directory
+        foreach (FileInfo file in dir.GetFiles())
+        {
+            string targetFilePath = Path.Combine(destinationDir, file.Name);
+            file.CopyTo(targetFilePath);
+        }
+
+        // If recursive and copying subdirectories, recursively call this method
+        if (recursive)
+        {
+            foreach (DirectoryInfo subDir in dirs)
+            {
+                string newDestinationDir = Path.Combine(destinationDir, subDir.Name);
+                CopyDirectory(subDir.FullName, newDestinationDir, true);
+            }
         }
     }
 
